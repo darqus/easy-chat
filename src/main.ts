6871ffd08707c718f68ad8f8426f65a794c36587
darqus <voladores@inbox.ru>
2025-07-01 22:53:24 +0300

    add title from env

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { APP_TITLE } from './constants/projectConstants'
+
+const documentTitle = APP_TITLE
+document.title = documentTitle
 
 createApp(App).mount('#app')
